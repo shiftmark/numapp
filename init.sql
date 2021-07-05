d4b13d4767c8b76fe4db1fbb09e56398e4b25a80
Adrian Bratu <16580387+shiftmark@users.noreply.github.com>
2021-07-05 17:48:33 +0300

    Updated default values note

--- init.sql
+++ init.sql
@@ -4,6 +4,6 @@ CREATE TABLE numapp (
     item_id VARCHAR (50) NOT NULL,
     duration INT NOT NULL);
 
--- Insert two default values
+-- Insert 4 default values
 INSERT INTO numapp (item_id, duration)
 VALUES ('a', 5), ('b', 12), ('c', 24), ('d', 48);
