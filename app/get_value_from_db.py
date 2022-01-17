af0f4ab0317676165dd8956cec14c4eb80ba4f84
Adrian Bratu <adrian.bratu@live.com>
2022-01-17 18:56:58 +0200

    added type hints and try exception

--- app/get_value_from_db.py
+++ app/get_value_from_db.py
@@ -2,29 +2,29 @@ import psycopg2 as pg
 import pandas as pd
 import os
 
-def get_time(item_id):
-    # Retreives the sleep duration value from the database.
+from typing import Union
 
-    numapp_df = None
+def get_time(item_id) -> Union[int, float]:
+    # Retrieves the sleep duration value from the database.
+
     connection = None
     
     # Save the database table in a dataframe and set the index to "item_id".
     try:  
         connection = pg.connect(
-            host = 'db',
-            database = os.environ['DB'],
-            user = 'postgres',
-            password = os.environ['DB_PASS']
+            host='db',
+            database=os.environ['DB'],
+            user='postgres',
+            password=os.environ['DB_PASS']
             )
         numapp_df = pd.read_sql_query('SELECT * FROM numapp', connection, index_col='item_id')
    
-    except:
-        raise
+    except Exception as error:
+        raise f"Get sleep time error - db connection failed: {error}"
     
     finally:
         if connection is not None:
             connection.close()
-            print('Connection to database closed.')
 
     return numapp_df.at[item_id, 'duration']
     
